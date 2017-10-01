f629ea4153f7288be3119cab45383c1896afd93d
Bruno <brmcerqueira@gmail.com>
2017-10-01 11:13:03 -0300

    Comi o cú de Lucas Simas...

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {Blocker} from '../../blocker';
+import {AngularFireDatabase, AngularFireObject} from 'angularfire2/database';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {fromPromise} from '../../observable.extensions';
 
 @Component({
   templateUrl: './player.component.html',
@@ -6,4 +11,17 @@ import {Component} from '@angular/core';
 })
 export class PlayerComponent {
 
+  public characterObservable: Observable<Character>;
+  private daoCharacter: AngularFireObject<any>;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private database: AngularFireDatabase,
+              private blocker: Blocker) {
+    this.daoCharacter = database.object(`characters/${this.activatedRoute.snapshot.params['key']}`);
+    this.characterObservable = this.daoCharacter.valueChanges();
+  }
+
+  public save(character: Character): void {
+    fromPromise(this.daoCharacter.update(character)).blocker(this.blocker).subscribe();
+  }
 }
