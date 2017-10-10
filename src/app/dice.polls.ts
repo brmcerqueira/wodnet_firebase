495ff38d3009fd55bbac13e37807aff73b531735
Bruno <brmcerqueira@gmail.com>
2017-10-09 21:17:14 -0300

    Ajuste...

--- src/app/dice.polls.ts
+++ src/app/dice.polls.ts
@@ -4,4 +4,8 @@ export const dicePolls: { [name: string]: (character: Character) => number } = {
   'dodging': c => c.dexterity + c.physical.dodge,
   'flee-strength': c => c.strength + c.physical.athletics,
   'flee-dexterity': c => c.dexterity + c.physical.athletics,
+  'surprise-attack': c => c.wits + c.physical.stealth,
+  'resistance-surprise-attack': c => c.perception + c.mental.awareness,
+  'close-combat-brawl': c => c.dexterity + c.physical.brawl,
+  'close-combat-melee': c => c.dexterity + c.physical.melee,
 };
